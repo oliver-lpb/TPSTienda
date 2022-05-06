178a4a269b5b9b93ec063fa37d240b4c3ced8539
oliver-lpb <82127783+oliver-lpb@users.noreply.github.com>
2022-05-05 20:27:43 -0600

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyDB98nDUjugfR4VmPEwHIttKiVbSUdUGF4",
+  
+    authDomain: "tarjetacredito2-a9715.firebaseapp.com",
+  
+    projectId: "tarjetacredito2-a9715",
+  
+    storageBucket: "tarjetacredito2-a9715.appspot.com",
+  
+    messagingSenderId: "259178604227",
+  
+    appId: "1:259178604227:web:dda437a82575a036372476",
+  
+    measurementId: "G-7RRH5XMKTH"
+  }
+};
